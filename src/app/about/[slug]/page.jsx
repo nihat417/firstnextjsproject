ddf16d25a2d3265a1a0fabab78b1a0604e156035
nihat417 <akreminihat@gmail.com>
2024-03-14 22:24:11 +0400

    first commit

---
+++ src/app/about/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeatilPage = () => {
+  return (
+    <div>DeatilPage</div>
+  )
+}
+
+export default DeatilPage;
---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hello from '@/components/about/page'
+
+const About = () => {
+  return (
+    <Hello/>
+  )
+}
+
+export default About;
